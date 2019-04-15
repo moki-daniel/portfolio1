9f53494eaede4b7fb56ccd2e7e7b93a1531f0b5a
moki-daniel <mokidnl@gmail.com>
2019-04-15 23:42:02 +0300

    [start #165339445] added the variables for the form

--- formtomail.js
+++ formtomail.js
@@ -1,11 +1,11 @@
 $('#Hire').submit(function(e){
-    var name = document.getElementById('inputFull-names')
-    var name = document.getElementById('e-mail')
-    var name = document.getElementById('inputFull-names')
-    var name = document.getElementById('inputFull-names')
-    var name = document.getElementById('inputFull-names')
-    var name = document.getElementById('inputFull-names')
-    var name = document.getElementById('inputFull-names')
-    var name = document.getElementById('inputFull-names')
+ var name = document.getElementById('inputFull-names'),
+     E-mail = document.getElementById('inputE-mail'),
+    Phone-number = document.getElementById('inputPhone-number'),
+    Subject = document.getElementById('inputSubject'),
+    Task-description = document.getElementById('inputTask-description'),
+    Project-timeline = document.getElementById('inputProject-timeline'),
+    Project-budget = document.getElementById('inputProject-budget');
+
 
 })
