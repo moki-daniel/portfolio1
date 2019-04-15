b4f9d8be964ab3366421562923f0a156c27192fe
moki-daniel <mokidnl@gmail.com>
2019-04-16 00:39:00 +0300

    [start #165339445] added the code needed to submit the form

--- formtomail.js
+++ formtomail.js
@@ -1,11 +1,29 @@
 $('#Hire').submit(function(e){
- var name = document.getElementById('inputFull-names'),
-     E-mail = document.getElementById('inputE-mail'),
-    Phone-number = document.getElementById('inputPhone-number'),
-    Subject = document.getElementById('inputSubject'),
-    Task-description = document.getElementById('inputTask-description'),
-    Project-timeline = document.getElementById('inputProject-timeline'),
-    Project-budget = document.getElementById('inputProject-budget');
 
+ var name = document.getElementById('inputName'),
+    email = document.getElementById('inputEmail'),
+    phonenumber = document.getElementById('inputPhonenumber'),
+    subject = document.getElementById('inputSubject'),
+    description = document.getElementById('inputDescription'),
+    timeline = document.getElementById('inputTimeline'),
+    budget = document.getElementById('inputBudget');
+    if (!name.value || !email.value || !phonenumber.value || !subject.value 
+        || !description.value || !timeline.value || !budget.value)
+        {
+        alertify.error('Please check your entries again')
+    }
+    else
+    {
+        //got from formspree for email direction
+        $.ajax({
+            url: "https://formspree.io/devmoki@gmail.com",
+            method: "POST",
+            data: $(this) .serialize(),
+            datatype: "json"
+        });
+        e.preventDefault()
+        $(this).get(0).reset()
+        alertify.success('message sent')
 
+    }
 })
