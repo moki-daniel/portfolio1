3a8371995f602bf3ae822c8c1a5059acbed89c76
moki-daniel <mokidnl@gmail.com>
2019-04-15 13:11:40 +0300

    [start #165339445] adding Js code for connection

---
+++ formtomail.js
@@ -0,0 +1,11 @@
+$('#Hire').submit(function(e){
+    var name = document.getElementById('inputFull-names')
+    var name = document.getElementById('e-mail')
+    var name = document.getElementById('inputFull-names')
+    var name = document.getElementById('inputFull-names')
+    var name = document.getElementById('inputFull-names')
+    var name = document.getElementById('inputFull-names')
+    var name = document.getElementById('inputFull-names')
+    var name = document.getElementById('inputFull-names')
+
+})
