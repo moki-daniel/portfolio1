b019cb4b0982ce59ddb4e5b06bde6ba4bb53b31d
moki-daniel <mokidnl@gmail.com>
2019-04-19 12:42:00 +0300

    [start #165431455] added function for testing the user inputs

--- form.php
+++ form.php
@@ -13,54 +13,60 @@
             <div class="card-header">
                 <h4 class="my-4">Hire me by filling the form below</h4>
  <!--Input fields-->
-            <div class="form-group">
-                <label for="inputName">Name</label>
-                <input type="text" class="form-control" placeholder="Enter your full-names" 
-                id="inputName" name="name">
-            </div>
-            <div class="form-group">
-                <label for="inputEmail">Email</label>
-                <input type="text" class="form-control" placeholder="Enter your E-mail"  
-                id="inputEmail" name="email">
-            </div>
-            <div class="form-group">
-                <label for="inputPhonenumber">Phone-number</label>
-                <input type="number" class="form-control" placeholder="Phone-number" 
-                id="inputPhonenumber" name="phonenumber">
-            </div>
-            <!--Subject-->
-            <div class="form-group">
-                <label for="inputSubject">Subject</label>
-                <textarea class="form-control" placeholder="Project title" id="inputSubject" 
-                name="subject"></textarea>
-            </div>
-            <!--Description-->
-            <div class="form-group">
-                <label for="inputDescription">Description</label>
-                <textarea class="form-control" placeholder="Describe the task to be done here" 
-                id="inputDescription" name="description"></textarea>
-            </div>
-            <!--Project timeline-->
-            <div class="form-group">
-                <label for="inputTimeline">Timeline</label>
-                <textarea class="form-control" placeholder="When should the project be completed?" 
-                id="inputTimeline" name="timeline"></textarea>
-            </div>
-            <!--Budget-->
-            <div class="form-group">
-                    <label for="inputBudget">Budget</label>
-                <textarea class="form-control" placeholder="What is your budget?" id="inputBudget" 
-                name="budget"></textarea>
-            </div>
-            <!--Button-->
-            <div class="form-group">
-                <button id="mailSubmit"class="btn btn-success text-white" name="submit">Submit</button>
-            </div>
-                    <p class="form-message"></p>
+                            <div class="form-group">
+                                <label for="inputName">Name</label>
+                                <input type="text" class="form-control" placeholder="Enter your full-names" 
+                                id="inputName" name="name">
+                                <span class="error"> <?= $name_error ?> </span>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail">Email</label>
+                                <input type="text" class="form-control" placeholder="Enter your E-mail"  
+                                id="inputEmail" name="email">
+                                <span class="error"> <?= $email_error ?> </span>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPhonenumber">Phone-number</label>
+                                <input type="number" class="form-control" placeholder="Phone-number" 
+                                id="inputPhonenumber" name="phonenumber">
+                                <span class="error"> <?= $phonenumber_error ?> </span>
+                            </div>
+                            <!--Subject-->
+                            <div class="form-group">
+                                <label for="inputSubject">Subject</label>
+                                <textarea class="form-control" placeholder="Project title" id="inputSubject" 
+                                name="subject"></textarea>
+                                <span class="error"> <?= $subject_error ?> </span>
+                            </div>
+                            <!--Description-->
+                            <div class="form-group">
+                                <label for="inputDescription">Description</label>
+                                <textarea class="form-control" placeholder="Describe the task to be done here" 
+                                id="inputDescription" name="description"></textarea>
+                                <span class="error"> <?= $description_error ?> </span>
+                            </div>
+                            <!--Project timeline-->
+                            <div class="form-group">
+                                <label for="inputTimeline">Timeline</label>
+                                <textarea class="form-control" placeholder="When should the project be completed?" 
+                                id="inputTimeline" name="timeline"></textarea>
+                                <span class="error"> <?= $timeline_error ?> </span>
+                            </div>
+                            <!--Budget-->
+                            <div class="form-group">
+                                    <label for="inputBudget">Budget</label>
+                                <textarea class="form-control" placeholder="What is your budget?" id="inputBudget" 
+                                name="budget"></textarea>
+                                <span class="error"> <?= $budget_error ?> </span>
+                            </div>
+                            <!--Button-->
+                            <div class="form-group">
+                                <button id="mailSubmit"class="btn btn-success text-white" name="submit">Submit</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </form>
--- form_process.php
+++ form_process.php
@@ -1,8 +1,62 @@
 <?php
 print_r($_POST);
-die;
+
 //define variables and set to empty values
 
 $name_error = $email_error = $phonenumber_error = $subject_error = $description_error = $timeline_error = $budget_error = "";
 $name = $email = $phonenumber = $subject = $description = $timeline = $budget = "";
+
+//form is sumbmitted with POST method
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])){
+        $name_error = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+
+    }
+    if (empty($_POST["email"])){
+        $email_error = "Name is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        //check if email is well formed
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_error = "Invalid email format";
+        }
+    }
+    if (empty($_POST[phonenumber]))
+    $phonenumber_error = "phone-number is required";
+    } else {  
+    $phonenumber = test_input($_POST["phonenumber"]);
+    }
+   }
+   if (empty($_POST[subject]))
+   $subject_error = "subject is required";
+   } else {  
+   $subject = test_input($_POST["subject"]);
+   }
+  }
+  if (empty($_POST[description]))
+  $description_error = "description is required";
+  } else {  
+  $description = test_input($_POST["description"]);
+  }
+ }
+ if (empty($_POST[timeline]))
+  $timeline_error = "Timeline for the project is required";
+  } else {  
+  $timeline = test_input($_POST["timeline"]);
+  }
+ }
+ if (empty($_POST[budget]))
+  $budget_error = "Budget for the project is required";
+  } else {  
+  $budget = test_input($_POST["budget"]);
+  }
+ }
+ function test_input($data){
+     $data = trim($data);
+     $data = stripslashes($data);
+     $data = htmlspecialchars($data);
+     return $data;
+ }
 ?>
